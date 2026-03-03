4573944ab5e42bfb0a179ea2d18d6802d8fe8a42
cloudemsNL <smarthost99@gmail.com>
2026-03-03 15:11:21 +0100

    Delete custom_components/cloudems directory

--- custom_components/cloudems/nilm/__init__.py
+++
@@ -1,7 +0,0 @@
-"""CloudEMS NILM package."""
-from .detector import NILMDetector, DetectedDevice
-from .database import NILMDatabase
-from .local_ai import LocalAIClassifier
-from .cloud_ai import CloudAIClassifier
-
-__all__ = ["NILMDetector", "DetectedDevice", "NILMDatabase", "LocalAIClassifier", "CloudAIClassifier"]
